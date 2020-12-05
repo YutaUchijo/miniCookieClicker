9f9d2a066400f97e2f21f2648fff0d402ebf0699
Yuta Uchijo <kgtnttki.yuta@gmail.com>
2020-12-05 21:01:09 +0900

    added description comments

--- src/main/kotlin/objects.kt
+++ src/main/kotlin/objects.kt
@@ -1,19 +1,19 @@
 class Game(){
-    var cookieNum = 0
-    var allCPS = 0
-    var baaNum = 0
-    var fieldNum = 0
-    var baaCPS = 0
-    var fieldCPS = 0
+    var cookieNum = 0   //現在のクッキー数
+    var allCPS = 0      //現在の一秒間に増えるクッキー数
+    var baaNum = 0      //おばあちゃんの人数
+    var fieldNum = 0    //畑の数
+    var baaCPS = 0      //おばあちゃんによって１秒間で増えるクッキー数
+    var fieldCPS = 0    //畑によって１秒間に増えるクッキー数
 
-    fun printCurr(){
+    fun printCurr(){    //現在の状態を表示する関数
         println("You have ${cookieNum} cookies now.")
         println("baa: ${baaNum}")
         println("field: ${fieldNum}")
         println("Current CPS: ${allCPS}")
     }
 
-    fun buy(tar:String, amo:Int){
+    fun buy(tar:String, amo:Int){ //おばあちゃんもしくは畑を買収する
         when(tar){
             "baa"   ->{
                 baaNum += amo
@@ -33,13 +33,13 @@ class Game(){
         }
     }
 
-    private fun refresh(){
+    private fun refresh(){  //allCPSとかを更新する関数。
         baaCPS = baaNum * 1
         fieldCPS = fieldNum * 5
         allCPS = baaCPS + fieldCPS
     }
 
-    fun click(){
+    fun click(){    //プレイヤーによるクリックの際の処理
         cookieNum++
     }
 }
